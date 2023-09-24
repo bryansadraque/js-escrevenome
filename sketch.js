bbd062fda9afeb38b542ff2e8d0ec5f0ffccf219
bryansadraque <134936872+bryansadraque@users.noreply.github.com>
2023-09-24 19:38:12 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 430);
+  background("blue")
+  }
+  
+  
+  function draw() {
+      
+   //console.log.(mouseispressed)
+    if(mouseIsPressed)
+    rect(mouseX,mouseY,20,30)
+    
+  }
